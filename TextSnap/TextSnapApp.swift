27a3da90d36fd4b918047dfb3878cfd5844445d4
Kohei Ishikawa <kouhei1@gmail.com>
2026-08-12 07:35:43 +0900

    Initial Commit

---
+++ TextSnap/TextSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextSnapApp.swift
+//  TextSnap
+//
+//  Created by toto on 2026/08/12.
+//
+
+import SwiftUI
+
+@main
+struct TextSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TextSnapTests/TextSnapTests.swift
@@ -0,0 +1,19 @@
+//
+//  TextSnapTests.swift
+//  TextSnapTests
+//
+//  Created by toto on 2026/08/12.
+//
+
+import Testing
+@testable import TextSnap
+
+struct TextSnapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
